544835d70e511cac82b14b425ad8f44b9a0a9319
zhaofeng-shu33 <616545598@qq.com>
2021-04-25 17:52:36 +0800

    update first

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ if [[ ! -z "$DEBIAN_MIRROR" ]]; then
    rm /etc/apt/sources.list
    echo "deb http://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free" > /etc/apt/sources.list
 fi
+apt update
 apt install texlive-latex-extra texlive-xetex texlive-fonts-recommended latexmk texlive-science
